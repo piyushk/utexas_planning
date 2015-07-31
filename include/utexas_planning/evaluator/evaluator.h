1b878359687f11868ee5cb963025d0d19e690d42
Piyush Khandelwal <piyushk@gmail.com>
2015-07-31 10:17:59 -0500

    starting up the evaluator class.

---
+++ include/utexas_planning/evaluator/evaluator.h
@@ -0,0 +1,17 @@
+#ifndef UTEXAS_PLANNING_EVALUATOR_H_
+#define UTEXAS_PLANNING_EVALUATOR_H_
+
+namespace utexas_planning {
+
+  class Evaluator {
+
+    public:
+      Evaluator ();
+    private:
+      /* data */
+
+  }; /* Evaluator */
+
+} /* utexas_planning */
+
+#endif /* end of include guard: UTEXAS_PLANNING_EVALUATOR_H_ */
