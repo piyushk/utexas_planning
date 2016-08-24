ba57081c43ce25ccc0714e44d96192fb6a53f101
Piyush Khandelwal <piyushk@gmail.com>
2016-08-24 13:39:42 -0500

    updated the runner.

--- condor/condor_runner.sh
+++ condor/condor_runner.sh
@@ -3,6 +3,6 @@
 # This command assumes all necessary shared object libraries have been loaded into
 export UTEXAS_PLANNING_LIBRARIES=/u/piyushk/utexas_planning/install/lib/libutexas_planning_models.so:/u/piyushk/utexas_planning/install/lib/libutexas_planning_planners.so
 export RDDL_DOMAIN_DIRECTORIES=/u/piyushk/utexas_planning/utexas_planning/benchmarks/rddl_prefix
-export LD_LIBRARY_PATH=/u/piyushk/utexas_planning/install/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=/u/piyushk/utexas_planning/install/lib:/u/piyushk/utexas_planning/install/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}
 
 ~/utexas_planning/install/bin/evaluator "$@"
