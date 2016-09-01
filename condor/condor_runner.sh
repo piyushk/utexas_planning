3b5c6d718c688c7a106849f2343cd1a752797223
Piyush Khandelwal <piyushk@gmail.com>
2016-09-01 15:41:36 -0500

    updated libraries in condor_runner script.

--- condor/condor_runner.sh
+++ condor/condor_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This command assumes all necessary shared object libraries have been loaded into
-export UTEXAS_PLANNING_LIBRARIES=/u/piyushk/utexas_planning/install/lib/libutexas_planning_models.so:/u/piyushk/utexas_planning/install/lib/libutexas_planning_planners.so
+export UTEXAS_PLANNING_LIBRARIES=/u/piyushk/utexas_planning/install/lib/
 export RDDL_DOMAIN_DIRECTORIES=/u/piyushk/utexas_planning/utexas_planning/benchmarks/rddl_prefix
 export LD_LIBRARY_PATH=/u/piyushk/utexas_planning/install/lib:/u/piyushk/utexas_planning/install/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}
 
